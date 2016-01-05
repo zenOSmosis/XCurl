c4c30263d42169c47801d42cb5ffd6a0efbff4e3
Jeremy Harris <jeremy.harris@zenosmosis.com>
2016-01-04 23:17:24 -0600

    Experimenting w/ PHP form

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+header('Test: 123');
+
+// Print request headers
+$headers = array();
+foreach($_SERVER as $key => $value) {
+    if (substr($key, 0, 5) <> 'HTTP_') {
+        continue;
+    }
+    $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+    $headers[$header] = $value;
+}
+print_r($headers);
