9c72effb103085873f3aa31feab4b42f3ecd3f44
Jeremy Harris <jeremy.harris@zenosmosis.com>
2016-01-07 16:52:47 -0600

    Added preliminary parsing of JSON config

--- src/headers/XCurlCLIInterpreter.hpp
+++ src/headers/XCurlCLIInterpreter.hpp
@@ -2,50 +2,42 @@
 #include "XCurl.hpp"
 #include "dist/jsoncpp.cpp"
 #include "dist/json/json.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
 
 class XCurlCLIInterpreter : public XCurl
 {
     public: XCurlCLIInterpreter(std::string configPath) : XCurl("")
 	{
-
-        // sniff out config file
         
-        FILE * pFile;
-        long lSize;
-        char * buffer;
-        size_t result;
+        Json::Value root;   // will contains the root value after parsing.
+        Json::Reader reader;
+        std::ifstream test(configPath, std::ifstream::binary);
+        bool parsingSuccessful = reader.parse(test, root, false);
+        if (!parsingSuccessful)
+        {
+            printf("Could not parse configuration file\n");
+            exit(1);
+        }
         
-        pFile = fopen (configPath.c_str() , "r" );
-        if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
-
-        // obtain file size:
-        fseek (pFile , 0 , SEEK_END);
-        lSize = ftell (pFile);
-        rewind (pFile);
-
-        // allocate memory to contain the whole file:
-        buffer = (char*) malloc (sizeof(char)*lSize);
-        if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
-
-        // copy the file into the buffer:
-        result = fread (buffer,1,lSize,pFile);
-        if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
-
-        /* the whole file is now loaded in the memory buffer. */
-        std::string bufferString = buffer;
-        printf("%s", bufferString.c_str());
+        std::string url = root.get("url", "ASCII").asString();      
+        this->setURL(url);
         
-        //Parse the buffer string
-        Json::Value root;
+  
+        Json::Value headers = root["headers"];
+        for (Json::ValueIterator itr = headers.begin(); itr != headers.end(); itr++)
+        {
+            Json::Value key = itr.key();
+            Json::Value value = (*itr);
+            
+            this->addRequestHeader(key.asString().c_str(), value.asString().c_str());
+        }
         
-        Json::Reader::parse(bufferString, root, true);
-
-
+        printf("%s", url.c_str());
         // terminate
-        fclose (pFile);
-        free (buffer);
-        
- 
-        exit(0);  // Temporary, for development
+        //fclose (pFile);
+        //free (buffer);
 	}
 };
